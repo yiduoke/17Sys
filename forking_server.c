3d088380ae7dea905631c6607b32e990fdc5034c
yiduoke <yiduokd822@jhs185.org>
2017-12-19 11:36:47 -0500

    main in server is from his previous hw

--- forking_server.c
+++ forking_server.c
@@ -11,8 +11,26 @@ static void sighandler(int signo) {
   }
 }
 
-int main() {
+// currently just his previous main he showed on the board
+int main(){
+	signal(SIGNIT, sighandler);
+
+	int to_client;
+	int from_client;
+	char buffer[BUFFER_SIZE];
+
+	while (1){
+		from_client = server_handshake(&to_client);
+	   while (read(from_client, buffer, sizeof(buffer))){//this can be a wrapped function for the subserver
+		printf("received: [%s] \n", buffer);
+		process(buffer);
+		write(to_client, buffer, sizeof(buffer));
+	   }
+	}
+	return 0;
+
 }
+
 
 void subserver(int from_client) {
 }
