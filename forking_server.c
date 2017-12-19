c765942b45c14834659a351fe5c3ce387fb67ce5
MdAbedin <mabedin1@stuy.edu>
2017-12-19 11:23:35 -0500

    added source files

---
+++ forking_server.c
@@ -0,0 +1,22 @@
+#include "pipe_networking.h"
+#include <signal.h>
+
+void process(char *s);
+void subserver(int from_client);
+
+static void sighandler(int signo) {
+  if (signo == SIGINT) {
+    remove("luigi");
+    exit(0);
+  }
+}
+
+int main() {
+}
+
+void subserver(int from_client) {
+}
+
+void process(char * s) {
+  s[0] = 'B'
+}
